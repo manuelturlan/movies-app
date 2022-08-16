301e26d5e8e4aba843678f9d1d623190773deefc
manuelturlan <manuelturlan18@gmail.com>
2022-08-16 13:29:05 -0400

    compose settings fixed

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from "../rootReducer";
 import thunk from "redux-thunk";
 
-const store = createStore(
-    rootReducer, 
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-)
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 export default store;
